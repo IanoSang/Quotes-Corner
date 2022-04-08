7172aa7fd836c694da0fd82ad09086488ab4325c
IanoSang <ianosang18@gmail.com>
2022-04-08 18:25:16 +0300

    feat: Output Property Binding- adds a toggle button which shows and hides quote details when clicked

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,5 +1,6 @@
 export class Quotes {
-  id!: number;
-  name!: string;
-  description!: string;
+  showDescription: boolean;
+  constructor(public id: number,public name: string,public description: string) {
+    this.showDescription = false;
+  }
 }
--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -10,13 +10,17 @@ import {Quotes} from "../quotes";
 })
 export class QuotesComponent implements OnInit {
   quotes:Quotes[] = [
-    {id:1, name:'Sometimes it falls upon a generation to be great, you can be that generation', description:'By Nelson Mandela'},
-    {id:2,name:'It\'s really that simple: Program, or be programmed', description:'By Software Dev'},
-    {id:3,name:'The greatest glory in living lies not in never falling, but in rising every time we fall', description:'By Unknown'},
-    {id:4,name:'The way to get started is to quit talking and begin doing', description:'By Someone'},
-    {id:5,name:'Life is what happens when you\'re busy making other plans.', description:'By ......'},
-    {id:6,name:'We live in a country where our young ladies who have recently attained the age of puberty cannot afford sanitary pads, but our men and women in public offices have iPads which they do not even know how to use.', description:'By Patrick Lumumba'},
+    new Quotes(1, 'Sometimes it falls upon a generation to be great, you can be that generation', 'By Nelson Mandela'),
+    new Quotes(2,'It\'s really that simple: Program, or be programmed', 'By Software Dev'),
+    new Quotes(3, 'The greatest glory in living lies not in never falling, but in rising every time we fall', 'By Unknown'),
+    new Quotes(4, 'The way to get started is to quit talking and begin doing', 'By Someone'),
+    new Quotes(4, 'The way to get started is to quit talking and begin doing', 'By Someone'),
+    new Quotes(6, 'We live in a country where our young ladies who have recently attained the age of puberty cannot afford sanitary pads, but our men and women in public offices have iPads which they do not even know how to use.', 'By Patrick Lumumba'),
   ];
+  toggleDetails(index: string | number){
+    // @ts-ignore
+    this.quotes[index].showDescription = !this.quotes[index].showDescription;
+  }
 
 
   constructor() { }
