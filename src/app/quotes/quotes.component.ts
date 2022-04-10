b6c7c1f1c1b7095bbfe506bffcfa4d348d7954dd
IanoSang <ianosang18@gmail.com>
2022-04-10 13:42:48 +0300

    feat: style-add styling to the buttons and edits the the values in the quotes array

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -11,11 +11,11 @@ import {Quotes} from "../quotes";
 export class QuotesComponent implements OnInit {
   quotes:Quotes[] = [
     new Quotes(1, 'Sometimes it falls upon a generation to be great, you can be that generation', 'By Nelson Mandela', 'Ian Sang', '0', '0'),
-    new Quotes(2, 'It\'s really that simple: Program, or be programmed', 'By Software Dev', 'Ian Sang', '0', '0',),
+    new Quotes(2, 'In the emerging highly programmed landscape ahead, you will either create the software or you will be the software, It\'s really that simple: Program, or be programmed', 'Douglas Rushoff', 'Ian Sang', '0', '0',),
     new Quotes(3, 'The greatest glory in living lies not in never falling, but in rising every time we fall', 'By Unknown', 'Ian Sang', '0', '0'),
-    new Quotes(4, 'The way to get started is to quit talking and begin doing', 'By Someone', 'Ian Sang', '0', '0'),
+    new Quotes(4, 'The way to get started is to quit talking and begin doing', 'Someone', 'Ian Sang', '0', '0'),
     new Quotes(5, 'Furaha sio kitu kilichoundwa tayari. Inakuja kutoka kwa matendo yako.', 'Swahili Quote', 'Ian Sang', '0', '0'),
-    new Quotes(6, 'We live in a country where our young ladies who have recently attained the age of puberty cannot afford sanitary pads, but our men and women in public offices have iPads which they do not even know how to use.', 'By Patrick Lumumba', 'Ian Sang', '0', '0'),
+    new Quotes(6, 'We live in a country where our young ladies who have recently attained the age of puberty cannot afford sanitary pads, but our men and women in public offices have iPads which they do not even know how to use.', 'Patrick Lumumba', 'Ian Sang', '0', '0'),
   ];
   toggleDetails(index: string | number){
     // @ts-ignore
