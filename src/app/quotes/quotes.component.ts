ce5cc812867daf2799ac420f1c6df89863ebf6df
IanoSang <ianosang18@gmail.com>
2022-04-13 00:03:36 +0300

    feat: style- adds styling to quote and quote details components

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -19,11 +19,11 @@ export class QuotesComponent implements OnInit,OnDestroy{
 
   quotes:Quotes[] = [
     new Quotes(1, 'Sometimes it falls upon a generation to be great, you can be that generation', 'Nelson Mandela', 'Ian Sang', 0, 0),
-    new Quotes(2, 'In the emerging highly programmed landscape ahead, you will either create the software or you will be the software, It\'s really that simple: Program, or be programmed', 'Douglas Rushoff', 'Ian Sang', 0, 0),
-    new Quotes(3, 'The greatest glory in living lies not in never falling, but in rising every time we fall', 'Unknown', 'Ian Sang', 0, 0),
-    new Quotes(4, 'The way to get started is to quit talking and begin doing', 'Someone', 'Ian Sang', 0, 0),
+    new Quotes(2, 'The greatest glory in living lies not in never falling, but in rising every time we fall', 'Unknown', 'Ian Sang', 0, 0),
     new Quotes(5, 'Furaha sio kitu kilichoundwa tayari. Inakuja kutoka kwa matendo yako.', 'Mswahili', 'Ian Sang', 0, 0),
-    new Quotes(6, 'We live in a country where our young ladies who have recently attained the age of puberty cannot afford sanitary pads, but our men and women in public offices have iPads which they do not even know how to use.', 'Patrick Lumumba', 'Ian Sang', 0, 0),
+    new Quotes(4, 'The way to get started is to quit talking and begin doing', 'Someone', 'Ian Sang', 0, 0),
+    new Quotes(6, 'Third World is a state of the mind and until we change our attitude as Africans, if there is a fourth, fifth and even sixth world, we will be in it.', 'Patrick Lumumba', 'Ian Sang', 0, 0),
+    new Quotes(3, 'In the emerging highly programmed landscape ahead, you will either create the software or you will be the software, It\'s really that simple: Program, or be programmed', 'Douglas Rushoff', 'Ian Sang', 0, 0),
   ];
   toggleDetails(index: string | number){
     // @ts-ignore
